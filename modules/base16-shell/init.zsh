5533d29e0e10d781838cc52afb6f270d9500d608
Marko Bauhardt <marko.bauhardt@mailbox.org>
2018-02-14 17:32:17 +0100

    added base16-shell as new module

---
+++ modules/base16-shell/init.zsh
@@ -0,0 +1,2 @@
+BASE16_SHELL=$DOTFILESDIR/modules/shell/externals/prezto/modules/base16-shell/external
+[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
