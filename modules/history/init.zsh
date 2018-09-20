3b0e07337d55bbc705d3f6a9383911ab8aec9c55
Marko Bauhardt <marko.bauhardt@mailbox.org>
2018-09-20 08:49:12 +0200

    updated some files after re-install and re-design folder structure in osx

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -10,7 +10,8 @@
 # Variables
 #
 
-HISTFILE="${ZDOTDIR:-$HOME}/.zhistory"       # The path to the history file.
+#HISTFILE="${ZDOTDIR:-$HOME}/.zhistory"       # The path to the history file.
+HISTFILE="${ZDOTDIR:-$HOME}/z/documents/histories/z_history"
 HISTSIZE=10000                   # The maximum number of events to save in the internal history.
 SAVEHIST=10000                   # The maximum number of events to save in the history file.
 
